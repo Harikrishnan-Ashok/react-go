e181cbb689dc22ab986e2fb5250eec06c0b25618
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-06-16 13:34:46 +0530

    navbar skeleton

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+	display:flex;
+	justify-content:space-between;
+	cursor: pointer;
+`;
+const Logo = styled.h1`
+	margin: 0px,10px;	
+`;
+const Links = styled.h3`
+	margin: 0px,10px;
+`;
+const LinkContainer = styled.div`
+	display:flex;
+	justify-content:space-around;
+`;
+
 export default function Navbar()
 {
 	return(
-		<>
-			<h1>This is the Navbar</h1>
-		</>
+		<Container>
+			<Logo> Test logo </Logo>
+			<LinkContainer>
+				<Links>link1</Links>
+				<Links>link2</Links>
+				<Links>link3</Links>
+			</LinkContainer>
+		</Container>
 	)	
 }
