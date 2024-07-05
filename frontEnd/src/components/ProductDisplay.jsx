f895e7caaf63303b927169b29d60fd95a09d902a
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-07-06 02:22:32 +0530

    productpage almost

--- frontEnd/src/components/ProductDisplay.jsx
+++ frontEnd/src/components/ProductDisplay.jsx
@@ -13,19 +13,78 @@ const ImageList=styled.div`
 	flex-direction:column;
 	gap: 16px;
 `;
-const Image=styled.div``;
-const Right=styled.div``;
-const H=styled.h1``;
-const Star=styled.div``;
+const Right=styled.div`
+	margin:0px 17px;
+	display:flex;
+	flex-direction:column;
+`;
+const H=styled.h1`
+	color:#3d3d3d;
+	font-size:40px;
+	font-weight:700px;
+`;
+const Star=styled.div`
+	display:flex;
+	align-items:center;
+	margin-top:13px;
+	gap:5px;color:#1c1c1c;
+	font-size:16px;
+
+`;
 const P=styled.div``;
-const Prices=styled.div``;
-const OldPrice=styled.div``;
-const NewPrice=styled.div``;
+const Prices=styled.div`
+	display:flex;
+	margin:40px 0px;
+	gap:30px;
+	font-size:24px;
+	font-weight:700px;
+	`;
+const OldPrice=styled.div`
+	color:#818181;
+	text-decoration:line-through;
+`;
+const NewPrice=styled.div`
+	color:#ff4141;
+
+`;
+const H2=styled.h1`
+	margin-top:55px;
+	font-size:20px;
+	color: #656565;
+	font-weight:600px;
+`;
 const SizeContainer=styled.div``;
-const Sizes=styled.div``;
+const SizeDiv=styled.div`
+	padding:18px 24px;
+	background:#fbfbfb;
+	border:1px solid #ebebeb;
+	border-radius:10px;
+	cursor:pointer;
+`;
+const Sizes=styled.div`
+	display:flex;
+	margin:30px 0px;
+	gap:20px;
+	
+`;
 const Desc=styled.div``;
-const Button=styled.button``;
-const Category=styled.div``;
+const Button=styled.button`
+	padding:20px 40px;
+	width:200px;
+	font-size:16px;
+	font-weight:600;
+	color:white;
+	margin-bottom:40px;
+	background:#FF4141;
+	border:none;
+	outline:none;
+	cursor:pointer;
+	border-radius:15px;
+
+`;
+const Category=styled.div`
+	margin-top:10px;
+`;
 
 import star_icon from "../components/assets/star_icon.png"
 import star_dull_icon from "../components/assets/star_dull_icon.png"
@@ -61,18 +120,18 @@ export default function ProductDisplay(props)
 					onnu poda vettavaliya descriptionum vayichoidn iricathe
 				</Desc>
 				<SizeContainer>
-					<H>Select Size</H>
+					<H2>Select Size</H2>
 					<Sizes>
-						<div>S</div>
-						<div>M</div>
-						<div>L</div>
-						<div>XL</div>
-						<div>XXL</div>
+						<SizeDiv>S</SizeDiv>
+						<SizeDiv>M</SizeDiv>
+						<SizeDiv>L</SizeDiv>
+						<SizeDiv>XL</SizeDiv>
+						<SizeDiv>XXL</SizeDiv>
 					</Sizes>
 				</SizeContainer>
 				<Button>Add to Kart</Button>
-				<Category><span>Category:</span>Women,Tshirt,CropTop</Category>
-				<Category><span>Tags:</span>Mordern,Latest</Category>
+				<Category><span style={{fontWeight:"600"}} >Category:</span>Women,Tshirt,CropTop</Category>
+				<Category><span style={{fontWeight:"600"}}>Tags:</span>Mordern,Latest</Category>
 			</Right>
 		</Container>
 	</>);
