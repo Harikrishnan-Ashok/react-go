859bd86e680c2631007c1827561fe8e767f692d3
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-07-08 23:11:03 +0530

    backEnd init
    Express is connected MongoDb is connected and the "/" end point is
    tested

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,8 +5,27 @@ const mongoose=require("mongoose");
 const jwt=require("jsonwebtoken");
 const multer=require("multer");
 const path = require("path")
-const cors= require("cors")
+const cors= require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// database conenction with mongo db 
+mongoose.connect("mongodb+srv://potbeater:Pots-Kada@cluster0.jjcdaab.mongodb.net/test");
+
+//API Creation
+app.get("/",(req,res)=>{
+	res.send("express is running")
+})
+
+
+app.listen(4000,(error)=>{
+	if (!error){
+		console.log("SERVER ODI KUTTA :"+port)
+	}
+	else
+	{
+		console.log(error)
+	}
+})
