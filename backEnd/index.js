d66ac1d860609942c2f14454d65b82578496a479
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-07-15 00:46:10 +0530

    lear

--- backEnd/index.js
+++ backEnd/index.js
@@ -6,6 +6,7 @@ const jwt=require("jsonwebtoken");
 const multer=require("multer");
 const path = require("path")
 const cors= require("cors");
+const {type} = require("os");
 
 const app = express();
 app.use(express.json());
@@ -38,7 +39,81 @@ app.post("/upload",upload.single("product"),(req,res)=>{
 	})
 })
 
+const Product= mongoose.model("Product",{
+	id:{
+		type:Number,
+		required:true,
+	},
+	name:{
+		type:String,
+		required:true,
+	},
+	image:{
+		type:String,
+		required:true,
+	},
+	category:{
+		type:String,
+		required:true,
+	},
+	new_price:{
+		type:Number,
+		required:true,
+	},
+	old_price:{
+		type:Number,
+		required:true,
+	},
+	date:{
+		type:Date,
+		default:Date.now,
+	},
+	avilable:{
+		type:Boolean,
+		default:true,
+	},
+})
 
+app.post("/addproduct",async(req,res)=>{
+	let products =await Product.find({})
+	let id;
+	if(Product.length>0)
+	{
+		let last_product_array=products.slice(-1);
+		let last_product = last_product_array[0];
+		id= last_product.id+1;
+	}
+	else
+	{
+		id=1;
+	}
+	const product = new Product({
+		id:id,
+		name:req.body.name,
+		image:req.body.image,
+		category:req.body.category,
+		new_price:req.body.new_price,
+		old_price:req.body.old_price,
+	});
+	console.log(product)
+	await product.save();
+	console.log("Saved succesfully");
+	res.json({
+		success:true,
+		name:req.body.name,
+	})
+})
+
+//removing products
+app.post("/removeProduct",async(req,res)=>{
+	await Product.findOneAndDelete({id:req.body.id});
+	console.log("removed")
+	res.json({
+		success:"true",
+		name:req.body.name
+	})
+})
+	
 app.listen(port,(error)=>{
 	if (!error){
 		console.log("SERVER ODI KUTTA :"+port)
